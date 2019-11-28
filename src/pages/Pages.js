2b2cc3361b2a6a7f075fcf8da6c96ba97fbef87d
zxzx93 <song-yeonsu@song-yeonsuui-MacBookPro.local>
2019-11-29 00:02:53 +0900

    page.js modified

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import ShopContainer from './shop/ShopContainer';
-import StoreContainer from './store/StoreContainer';
-import StoreDetail from './store/StoreDetail';
-import ShopDetailContainer from './shop/ShopDetailContainer';
-import CartConatiner from './cart/CartConatiner';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import ShopContainer from "./shop/ShopContainer";
+import StoreContainer from "./store/StoreContainer";
+import StoriesContainer from "./stories/StoriesContainer";
+import StoreDetail from "./store/StoreDetail";
+import ShopDetailContainer from "./shop/ShopDetailContainer";
+import CartConatiner from "./cart/CartConatiner";
 
 class Pages extends Component {
   render() {
     return (
-      <div style={{ paddingTop: '80px' }}>
+      <div style={{ paddingTop: "80px" }}>
         <Route path="/shop" exact component={ShopContainer} />
         <Route path="/shop_detail" exact component={ShopDetailContainer} />
         <Switch>
@@ -17,6 +18,7 @@ class Pages extends Component {
           <Route path="/store" exact component={StoreContainer} />
         </Switch>
         <Route path="/cart" exact component={CartConatiner} />
+        <Route path="/stories" exact component={StoriesContainer} />
       </div>
     );
   }
